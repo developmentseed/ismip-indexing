6bffdc6fb7cea4805949be5eaf238433b5a98742
Aimee Barciauskas <aimee@developmentseed.org>
2025-11-27 12:17:56 -0800

    Add add token: anon for gcs fs

--- ismip6_helper/index.py
+++ ismip6_helper/index.py
@@ -96,7 +96,7 @@ def build_file_index(bucket: str = "ismip6",
     print(f"Building file index from gs://{bucket}...")
 
     # Initialize filesystem
-    fs = fsspec.filesystem('gs', anon=True)
+    fs = fsspec.filesystem('gs', storage_options={'token': 'anon'})
 
     # Collect all .nc files with their metadata
     all_files = []
